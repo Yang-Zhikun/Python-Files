eb46828b1a89f08a1c18d4f293776474bcf74f5c
404 not found <124446532+Yang-Zhikun@users.noreply.github.com>
2025-07-30 03:40:45 +0000

    哈希

--- 数据结构/哈希表/Array_hash_map.py
+++ 数据结构/哈希表/Array_hash_map.py
@@ -1,20 +1,43 @@
 """基于数组实现的哈希表"""
-class Array_hash_map:
-    """键值对"""
-    class Pair:
-        def __init__(self, key, val):
-            self.key = key
-            self.val = val
+"""键值对"""
+class Pair:
+    def __init__(self, key: int, val):
+        self.key: int = key
+        self.val = val
 
+class Array_hash_map:
     def __init__(self, length: int = 101):
         """构造一个长度为length的数组"""
-        self.length = length
+        self.length: int = length
 
-        self.data: list[self.Pair | None] = [None] * length
+        self.data: list[Pair | None] = [None] * length
 
-    def put(self, key, val):
+    def hash_func(self, key: int):
+        """哈希函数, 取余法"""
+        return int(key) % self.length
+    
+    def put(self, key: int, val):
         """插入键值对"""
-        pair = self.Pair(key, val)
+        pair: Pair = Pair(key, val)
+        index: int = self.hash_func(key) # 通过哈希函数获取储存索引
+        self.data[index] = pair
+    
+    def get(self, key: int):
+        """通过key查询val"""
+        index: int = self.hash_func(key) # 通过哈希函数获取索引
+        if self.data[index] != None:
+            return self.data[index].val
+        else:
+            return None
+
+    def printList(self):
+        """打印哈希表"""
+        for data in self.data:
+            if data != None:
+                print("key:", data.key, "->val:", data.val)
+    
+    
+        
         
 
 
@@ -22,3 +45,16 @@ class Array_hash_map:
 if __name__ == "__main__":
     hash = Array_hash_map()
     print("哈希表长度：", hash.length)
+    hash.put(123456, "name1")
+    hash.put(123350, "name2")
+    hash.put(23456, "name3")
+
+    print("打印哈希表")
+    hash.printList()
+    print()
+
+    print("查询：123456->", hash.get(123456))
+    print("查询：123350->", hash.get(123350))
+    print("查询：123213->", hash.get(123213))
+    print("查询：23456 ->", hash.get(23456))
+    
