6f3952e71b4ef1f75aff52d0c986088be06f36fd
404 not found <124446532+Yang-Zhikun@users.noreply.github.com>
2025-07-30 00:41:41 +0000

    hash_map

---
+++ 数据结构/哈希表/Array_hash_map.py
@@ -0,0 +1,16 @@
+"""基于数组实现的哈希表"""
+class Array_hash_map:
+    """键值对"""
+    class Pair:
+        def __init__(self, key, val):
+            self.key = key
+            self.val = val
+
+    def __init__(self, length: int = 101):
+        """构造一个长度为length的数组"""
+        self.data: list[Pair | None] = [None] * length
+
+    def put(self, key, val):
+        """插入键值对"""
+        pair = self.Pair(key, val)
+        
