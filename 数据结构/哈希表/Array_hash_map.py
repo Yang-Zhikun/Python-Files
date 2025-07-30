65a1d874bf0dd5c807f815bda6869e4a1800fc12
404 not found <124446532+Yang-Zhikun@users.noreply.github.com>
2025-07-30 00:46:42 +0000

    update

--- 数据结构/哈希表/Array_hash_map.py
+++ 数据结构/哈希表/Array_hash_map.py
@@ -8,9 +8,17 @@ class Array_hash_map:
 
     def __init__(self, length: int = 101):
         """构造一个长度为length的数组"""
-        self.data: list[Pair | None] = [None] * length
+        self.length = length
+
+        self.data: list[self.Pair | None] = [None] * length
 
     def put(self, key, val):
         """插入键值对"""
         pair = self.Pair(key, val)
         
+
+
+
+if __name__ == "__main__":
+    hash = Array_hash_map()
+    print("哈希表长度：", hash.length)
