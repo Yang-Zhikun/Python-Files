3b7145948ab2f084e4612f1383e0a762a3e6b1e6
404 not found <776560548@qq.com>
2025-10-26 16:27:15 +0800

    update

--- class_test.py
+++ class_test.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
         self.price = price
     def get_info(self):
-        return f"《{self.title}, 作者：{self.author}, 价格：{self.price}元》"
+        return f"《{self.title}》, 作者：{self.author}, 价格：{self.price}元"
 
 Book1 = Book("Python编程", "张三", 99.99)
 Book2 = Book("数据结构与算法", "李四", 89.99)
